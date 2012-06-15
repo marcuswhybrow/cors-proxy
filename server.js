8d86a5aa1669580a7d3f8eac085c95bbc8e0319f
Marcus Whybrow <marcus@marcuswhybrow.net>
2012-06-15 21:17:13 +0100

    Added raw option un URL, removed header options.
    raw mode returns all files in "text/plain; charset=x-user-defined"
    format, and is enabled by sending the url to /raw/

--- server.js
+++ server.js
@@ -12,68 +12,75 @@ var http = require('http'),
 
 var server = http.createServer(function(req, res) {
   
-  var path = req.url.slice(1);
+  function getDocument(path, raw) {
+    var proxyUrl = url.parse(path),
+        options = {
+          host: proxyUrl.hostname,
+          port: proxyUrl.port || 80,
+          path: proxyUrl.path,
+          method: 'GET'
+        };
+
+    function validContentType(type) {
+      var re = new RegExp('(' + invalidContentTypes.join('|') + ')');
+      return ! re.test(type);
+    }
+
+    function handleRes(proxyRes) {
+      if ([301, 302, 307, 308].indexOf(proxyRes.statusCode) >= 0 && proxyRes.headers['location']) {
+        var location = url.parse(proxyRes.headers['location']),
+            options = {
+              host: location.host,
+              post: location.port || 80,
+              path: location.path,
+              method: 'GET'
+            };
+          http.request(options, handleRes).end();
+      } else {
+        if (validContentType(proxyRes.headers['content-type'])) {
+          var headers = proxyRes.headers;
+          if (raw)
+            headers['content-type'] = 'text/plain; charset=x-user-defined';
+          for (name in corsHeaders)
+            headers[name] = corsHeaders[name];
+          res.writeHead(proxyRes.statusCode, headers);
+
+          proxyRes.on('data', function(chunk) {
+            res.write(chunk, 'binary');
+          });
+          proxyRes.on('end', function() {
+            res.end();
+          });
+        } else {
+          res.statusCode = 415 // Unsupported Media Type
+          res.end();
+        }
+      }
+    }
+
+    console.log(options.host, options.method, options.path);
+
+    var proxy = http.request(options, handleRes);
+    proxy.on('error', function() {
+      console.log('error: ' + options);
+      res.end();
+    });
+
+    proxy.end();
+  }
   
-  if (path.indexOf('http://') != 0) {
+  console.log(req.url);
+  
+  if (req.url.indexOf('/http://') == 0) {
+    getDocument(req.url.slice(1));
+  } else if (req.url.indexOf('/raw/http://') == 0) {
+    console.log(req.url.slice(5));
+    getDocument(req.url.slice(5), true);
+  } else {
     res.statusCode = 404
     res.end();
     return
   }
-  
-  var proxyUrl = url.parse(path),
-      options = {
-        host: proxyUrl.hostname,
-        port: proxyUrl.port || 80,
-        path: proxyUrl.path,
-        method: 'GET'
-      };
-  
-  function validContentType(type) {
-    var re = new RegExp('(' + invalidContentTypes.join('|') + ')');
-    return ! re.test(type);
-  }
-  
-  function handleRes(proxyRes) {
-    if ([301, 302, 307, 308].indexOf(proxyRes.statusCode) >= 0 && proxyRes.headers['location']) {
-      var location = url.parse(proxyRes.headers['location']),
-          options = {
-            host: location.host,
-            post: location.port || 80,
-            path: location.path,
-            method: 'GET'
-          };
-        http.request(options, handleRes).end();
-    } else {
-      if (validContentType(proxyRes.headers['content-type'])) {
-        var headers = proxyRes.headers;
-        if (req.headers['x-corsproxy-override-mime-type'])
-          headers['content-type'] = req.headers['x-corsproxy-override-mime-type']
-        for (name in corsHeaders)
-          headers[name] = corsHeaders[name];
-        res.writeHead(proxyRes.statusCode, headers);
-
-        proxyRes.on('data', function(chunk) {
-          res.write(chunk, 'binary');
-        });
-        proxyRes.on('end', function() {
-          res.end();
-        });
-      } else {
-        res.statusCode = 415 // Unsupported Media Type
-        res.end();
-      }
-    }
-  }
-  
-  console.log(options.host, options.method, options.path);
-  
-  var proxy = http.request(options, handleRes);
-  proxy.on('error', function() {
-    console.log('error: ' + options);
-    res.end();
-  });
-  
-  proxy.end();
 });
 
 var port = process.env.PORT || 5000;
